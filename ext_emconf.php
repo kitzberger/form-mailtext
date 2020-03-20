6a67c1ab8388b51e4e3120d8152c355135c0c25f
Philipp Kitzberger <philipp@kitze.net>
2020-03-20 11:26:37 +0100

    FIX: version constraints for TER compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '1.0.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-',
-			'form' => '8.7.0-'
+			'typo3' => '8.7.0-9.5.99',
+			'form' => '8.7.0-9.5.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
