9ecccf83bf99ed57770f8737e930bc1b9dd03fe9
Philipp Kitzberger <philipp@kitze.net>
2020-12-03 13:03:35 +0100

    FIX: EmailFinisher override not working anymore

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,20 +11,19 @@ call_user_func(function () {
                     }
                 }
             }
-        ')
-    );
-    if (TYPO3_MODE === 'BE') {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-            trim('
-                module.tx_form {
-                    settings {
-                        yamlConfigurations {
-                            100 = EXT:form_mailtext/Configuration/Form/MailtextFormSetup.yaml
-                        }
+            module.tx_form {
+                settings {
+                    yamlConfigurations {
+                        100 = EXT:form_mailtext/Configuration/Form/MailtextFormSetup.yaml
                     }
                 }
-            ')
-        );
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][] = 'EXT:form_mailtext/Resources/Private/Language/Database.xlf';
-    }
+            }
+        ')
+    );
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][] = 'EXT:form_mailtext/Resources/Private/Language/Database.xlf';
+
+    # Override EmailFinisher with slightly different one (that handles 'message' field)
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Form\Domain\Finishers\EmailFinisher::class] = [
+        'className' => \Kitzberger\FormMailtext\Domain\Finishers\EmailFinisher::class
+    ];
 });
