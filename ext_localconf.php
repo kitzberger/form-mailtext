aef8a79c438e1bd174e0d7bb8aed1e0efabbbaf4
Philipp Kitzberger <philipp@kitze.net>
2020-12-09 21:21:26 +0100

    Put TS into more obvious location: ext_typoscript_setup.typoscript

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,24 +2,6 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        trim('
-            plugin.tx_form {
-                settings {
-                    yamlConfigurations {
-                        100 = EXT:form_mailtext/Configuration/Form/MailtextFormSetup.yaml
-                    }
-                }
-            }
-            module.tx_form {
-                settings {
-                    yamlConfigurations {
-                        100 = EXT:form_mailtext/Configuration/Form/MailtextFormSetup.yaml
-                    }
-                }
-            }
-        ')
-    );
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][] = 'EXT:form_mailtext/Resources/Private/Language/Database.xlf';
 
     # Override EmailFinisher with slightly different one (that handles 'message' field)
