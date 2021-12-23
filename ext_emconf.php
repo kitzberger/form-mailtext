a02139c27056a3981ff13c45dc3afe70fd965369
Philipp Kitzberger <coding@kitze.net>
2021-12-23 18:46:41 +0100

    Finetune version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.1.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '10.4.0-11.5.99',
-			'form' => '10.4.0-11.5.99',
+			'typo3' => '10.4.11-11.5.99',
+			'form' => '10.4.11-11.5.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
